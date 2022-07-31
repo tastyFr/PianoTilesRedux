b3682d5cbaf677c4bd7fafa05356e82e4b1ca771
tastyForReal <40455776+tastyFr@users.noreply.github.com>
2022-07-31 10:16:51 +0800

    Make variable name shorter  + removed unused ones

--- PianoTilesRedux.Game/PianoTilesReduxGame.cs
+++ PianoTilesRedux.Game/PianoTilesReduxGame.cs
@@ -10,14 +10,13 @@ using osu.Framework.Screens;
 using osuTK;
 using osuTK.Graphics;
 using PianoTilesRedux.Game.Elements;
-using PianoTilesRedux.Game.Screens;
+using PianoTilesRedux.Game.Screens.Startup;
 
 namespace PianoTilesRedux.Game
 {
     public class PianoTilesReduxGame : PianoTilesReduxGameBase
     {
-        private ScreenStack screen;
-        private SafeAreaDefiningContainer safeAreaContainer;
+        private SafeAreaDefiningContainer container;
 
         [BackgroundDependencyLoader]
         private void load()
@@ -25,14 +24,18 @@ namespace PianoTilesRedux.Game
             Add(new SplashBackground { RelativeSizeAxes = Axes.Both });
             AddInternal(
                 // A safe area container is defined here for the portrait screen.
-                safeAreaContainer = new SafeAreaDefiningContainer
+                container = new SafeAreaDefiningContainer
                 {
                     Anchor = Anchor.TopCentre,
                     Origin = Anchor.TopCentre,
                     Children = new Drawable[]
                     {
                         new Box { RelativeSizeAxes = Axes.Both, Colour = Color4.Black },
-                        screen = new ScreenStack(new FirstTimeScreen()) { RelativeSizeAxes = Axes.Both }
+                        new DrawSizePreservingFillContainer
+                        {
+                            TargetDrawSize = new Vector2(540, 960),
+                            Child = new ScreenStack(new Disclaimer()) { RelativeSizeAxes = Axes.Both }
+                        }
                     }
                 }
             );
@@ -45,7 +48,7 @@ namespace PianoTilesRedux.Game
             // Update the safe area container's size to match the current screen
             // size in portrait mode.
             var newSize = new Vector2(BoundingBox.Width, BoundingBox.Height);
-            safeAreaContainer.Size = new Vector2(Math.Min(newSize.Y / 16f * 9f, newSize.X), newSize.Y);
+            container.Size = new Vector2(Math.Min(newSize.Y / 16f * 9f, newSize.X), newSize.Y);
         }
     }
 }
