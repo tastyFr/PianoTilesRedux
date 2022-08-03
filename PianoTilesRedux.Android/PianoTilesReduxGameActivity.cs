04926daee34fb525fa76ccd36d1c1a70013b9932
tastyForReal <40455776+tastyFr@users.noreply.github.com>
2022-08-04 03:46:16 +0800

    Add Android project

---
+++ PianoTilesRedux.Android/PianoTilesReduxGameActivity.cs
@@ -0,0 +1,23 @@
+﻿// Piano Tiles Redux:
+// Made by tastyForReal (2022)
+
+using Android.App;
+using osu.Framework.Android;
+using PianoTilesRedux.Game;
+
+namespace PianoTilesRedux.Android
+{
+    [Activity(
+        ConfigurationChanges = DEFAULT_CONFIG_CHANGES,
+        Exported = true,
+        LaunchMode = DEFAULT_LAUNCH_MODE,
+        MainLauncher = true
+    )]
+    public class PianoTilesReduxGameActivity : AndroidGameActivity
+    {
+        protected override osu.Framework.Game CreateGame()
+        {
+            return new PianoTilesReduxGame();
+        }
+    }
+}
---
+++ PianoTilesRedux.Android/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿// Piano Tiles Redux:
+// Made by tastyForReal (2022)
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("PianoTilesRedux.Android")]
+[assembly: AssemblyCompany("tastyForReal")]
+[assembly: AssemblyProduct("PianoTilesRedux.Android")]
