ca91a55d02e4769a32b0ddbf42890deb0babfe09
tastyForReal <40455776+tastyFr@users.noreply.github.com>
2022-08-21 06:25:52 +0800

    Update `SongSelect` test

---
+++ PianoTilesRedux.Game.Tests/Resources/TestResources.cs
@@ -0,0 +1,12 @@
+// Piano Tiles Redux:
+// Made by tastyForReal (2022)
+
+using System.Reflection;
+
+namespace PianoTilesRedux.Game.Tests.Resources
+{
+    public static class TestResources
+    {
+        public static Assembly ResourceAssembly => typeof(TestResources).Assembly;
+    }
+}
--- PianoTilesRedux.Game.Tests/Visual/TestSceneSongSelect.cs
+++ PianoTilesRedux.Game.Tests/Visual/TestSceneSongSelect.cs
@@ -2,62 +2,107 @@
 // Made by tastyForReal (2022)
 
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.IO.Stores;
 using osu.Framework.Screens;
 using PianoTilesRedux.Game.Levels;
 using PianoTilesRedux.Game.Screens.Select;
 using PianoTilesRedux.Game.Screens.Select.Carousel;
+using PianoTilesRedux.Game.Tests.Resources;
 
 namespace PianoTilesRedux.Game.Tests.Visual
 {
     [TestFixture]
     public class TestSceneSongSelect : PianoTilesReduxTestScene
     {
-        private const string long_title = "This title has an implausibly long title that will overflow";
-        private const string long_artist_name =
-            "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+        private ResourceStore<byte[]> resources = null!;
+
+        private readonly List<ILevelInfo> levels = new();
 
         private readonly ScreenStack screenStack;
         private SongSelect songSelect;
-        private readonly List<ILevelInfo> levels = new();
 
         public TestSceneSongSelect()
         {
-            levels.Add(new LevelInfo { Title = long_title, Artist = long_artist_name });
-            Add(screenStack = new ScreenStack());
+            Add(screenStack = new());
         }
 
         [BackgroundDependencyLoader]
         private void load()
         {
-            screenStack.Push(songSelect = new SongSelect());
+            resources = new(new DllResourceStore(TestResources.ResourceAssembly));
+            screenStack.Push(songSelect = new());
         }
 
         [Test]
-        public void TestAssertSongSelectLoaded()
+        public void TestLoaded()
         {
-            AddAssert("SongSelect loaded", () => songSelect.LoadState == LoadState.Loaded);
+            Assert.AreEqual(songSelect.LoadState, LoadState.Loaded);
+            Assert.IsTrue(resources.GetAvailableResources().Any());
+        }
+
+        private void deserializeAndAddLevels()
+        {
+            levels.Clear();
+            Assert.IsEmpty(levels);
+
+            using var level = resources.GetStream("Resources/Levels.json");
+            string json = new StreamReader(level).ReadToEnd();
+            var levelsList = getLevelsList(json);
+
+            Assert.IsNotEmpty(levelsList);
+
+            foreach (var levelInfo in levelsList)
+            {
+                levels.Add(levelInfo);
+            }
+
+            for (int i = 0; i < levels.Count; i++)
+            {
+                Assert.AreEqual(i + 1, levels[i].Id, $"Level ID {i + 1} is not sequential");
+            }
         }
 
         [Test]
         public void TestAddLevels()
         {
+            _ = AddStep("deserialize and add levels", () => deserializeAndAddLevels());
+
+            AddUntilStep("check if levels were added", () => levels.Any());
+
             _ = AddStep("clear levels", () => songSelect.Levels.Clear());
+
             AddAssert("no levels", () => !songSelect.Levels.OfType<LevelCarousel>().Any());
-            _ = AddStep("add levels", () => addLevels(1000000, levels.First().Title, levels.First().Artist));
-            AddAssert("has three levels", () => songSelect.Levels.OfType<LevelCarousel>().Count() == 3);
+
+            _ = AddStep(
+                $"add first level",
+                () => addLevel(levels.First().Id, levels.First().Title, levels.First().Artist)
+            );
+
+            _ = AddStep(
+                $"add last level",
+                () => addLevel(levels.Last().Id, levels.Last().Title, levels.Last().Artist)
+            );
+
+            AddAssert("levels added", () => songSelect.Levels.OfType<LevelCarousel>().Any());
+
             AddAssert("last level is SpriteText", () => songSelect.Levels[^1] is SpriteText);
         }
 
-        private void addLevels(int level, string title, string artist)
+        private void addLevel(int level, string title, string artist)
         {
             songSelect.Levels.Add(new LevelCarousel(level) { Title = title, Artist = artist });
-            songSelect.Levels.Add(new LevelCarousel(level) { Title = title, Artist = artist });
-            songSelect.Levels.Add(new LevelCarousel(level) { Title = title, Artist = artist });
+        }
+
+        private static List<LevelInfo> getLevelsList(string json)
+        {
+            return JsonConvert.DeserializeObject<List<LevelInfo>>(json);
         }
     }
 }
