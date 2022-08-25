462fcfec7237e0eda84d46713569fa500ad25ed8
tastyForReal <40455776+tastyFr@users.noreply.github.com>
2022-08-25 09:33:03 +0800

    Add a simple test case for `LevelInfo`

---
+++ PianoTilesRedux.Game.Tests/NonVisual/LevelInfoTest.cs
@@ -0,0 +1,62 @@
+﻿// Piano Tiles Redux:
+// Made by tastyForReal (2022)
+
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using osu.Framework.Testing;
+using PianoTilesRedux.Game.Levels;
+
+namespace PianoTilesRedux.Game.Tests.NonVisual
+{
+    [HeadlessTest]
+    public class LevelInfoTest
+    {
+        private readonly TemporaryNativeStorage storage;
+        private readonly LevelInfo levelInfo;
+
+        public LevelInfoTest()
+        {
+            storage = new TemporaryNativeStorage(Guid.NewGuid().ToString());
+
+            levelInfo = new LevelInfo
+            {
+                Id = 1,
+                Title = "Little Star",
+                Artist = "Traditional",
+                Bpms = new float[3] { 85, 90, 94 },
+                Difficulty = Difficulty.VeryEasy,
+                UnlockType = UnlockType.PlayerLevel,
+                Source = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            };
+        }
+
+        private void serialize()
+        {
+            using var stream = storage.CreateFileSafely("LittleStar.json");
+            using var writer = new StreamWriter(stream);
+            writer.WriteLine(JsonConvert.SerializeObject(levelInfo));
+        }
+
+        [Test]
+        public void TestDeserialize()
+        {
+            Assert.DoesNotThrow(serialize);
+
+            using var reader = new StreamReader(storage.GetStream("LittleStar.json"));
+            Assert.NotNull(reader);
+
+            string value = reader.ReadToEnd();
+            var data = JsonConvert.DeserializeObject<LevelInfo>(value);
+
+            Assert.AreEqual(levelInfo.Id, data.Id);
+            Assert.AreEqual(levelInfo.Title, data.Title);
+            Assert.AreEqual(levelInfo.Artist, data.Artist);
+            Assert.AreEqual(levelInfo.Bpms, data.Bpms);
+            Assert.AreEqual(levelInfo.Difficulty, data.Difficulty);
+            Assert.AreEqual(levelInfo.UnlockType, data.UnlockType);
+            Assert.AreEqual(levelInfo.Source, data.Source);
+        }
+    }
+}
