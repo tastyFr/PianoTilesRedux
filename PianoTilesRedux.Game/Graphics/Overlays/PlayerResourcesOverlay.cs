be5de9f420bf61025caa38613b40117db62d8b8f
tastyForReal <40455776+tastyFr@users.noreply.github.com>
2022-09-01 12:08:29 +0800

    Update `PlayerResourcesOverlay` to fit design width

--- PianoTilesRedux.Game/Graphics/Overlays/PlayerResourcesOverlay.cs
+++ PianoTilesRedux.Game/Graphics/Overlays/PlayerResourcesOverlay.cs
@@ -1,6 +1,7 @@
 ﻿// Piano Tiles Redux:
 // Made by tastyForReal (2022)
 
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osuTK;
@@ -8,29 +9,43 @@ using PianoTilesRedux.Game.Graphics.Overlays.ResourcesOverlay;
 
 namespace PianoTilesRedux.Game.Graphics.Overlays
 {
-    public class PlayerResourcesOverlay : FillFlowContainer
+    public class PlayerResourcesOverlay : Container
     {
-        private readonly Bar xpBar;
-        private readonly Bar livesBar;
-        private readonly Bar goldBar;
-        private readonly Bar diamondsBar;
+        private Bar xpBar;
+        private Bar livesBar;
+        private Bar goldBar;
+        private Bar diamondsBar;
+
+        private FillFlowContainer fillFlowContainer;
 
         public PlayerResourcesOverlay()
+        {
+            Anchor = Anchor.TopCentre;
+            Origin = Anchor.TopCentre;
+            Margin = new MarginPadding { Top = 13 };
+            AutoSizeAxes = Axes.Y;
+            Width = PianoTilesReduxGame.DesignResolution.X;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
         {
             xpBar = new Bar(BarType.Xp);
             livesBar = new Bar(BarType.Lives);
             goldBar = new Bar(BarType.Gold);
             diamondsBar = new Bar(BarType.Diamonds);
 
-            AutoSizeAxes = Axes.Both;
-            Anchor = Anchor.TopCentre;
-            Origin = Anchor.TopCentre;
+            fillFlowContainer = new FillFlowContainer
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                AutoSizeAxes = Axes.Both,
+                Direction = FillDirection.Horizontal,
+                Spacing = new Vector2(10, 0),
+                Children = new[] { xpBar, livesBar, goldBar, diamondsBar }
+            };
 
-            Direction = FillDirection.Horizontal;
-            Spacing = new Vector2(10, 0);
-            Margin = new MarginPadding { Top = 13 };
-
-            InternalChildren = new[] { xpBar, livesBar, goldBar, diamondsBar };
+            AddInternal(fillFlowContainer);
         }
     }
 }
