ee811098bfdb4ca8c3bf12f19d7db0562a197468
tastyForReal <40455776+tastyFr@users.noreply.github.com>
2022-09-09 15:14:21 +0800

    Add home screen navigation

---
+++ PianoTilesRedux.Game.Tests/Visual/Home/TestSceneHomeBottomNavigation.cs
@@ -0,0 +1,51 @@
+﻿// Piano Tiles Redux:
+// Made by tastyForReal (2022)
+
+using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Testing.Input;
+using osuTK;
+using osuTK.Input;
+using PianoTilesRedux.Game.Screens.Home;
+using PianoTilesRedux.Game.Screens.Home.HomeNavigation;
+
+namespace PianoTilesRedux.Game.Tests.Visual.Home
+{
+    [TestFixture]
+    public class TestSceneHomeBottomNavigation : PianoTilesReduxTestScene
+    {
+        private HomeBottomNavigation navigation;
+        private ManualInputManager inputManager;
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            navigation = new();
+            inputManager = new() { Child = navigation };
+            Add(inputManager);
+            navigation.OnUpdate += _ =>
+                Schedule(() => navigation.Scale = new Vector2(Content.DrawSize.X / navigation.DrawSize.X));
+        }
+
+        [Test]
+        public void TestNavigate()
+        {
+            _ = AddStep("select music", () => select(1));
+            AddAssert("music selected", () => navigation.SelectedTab == NavigationTab.Music);
+            _ = AddStep("select hall", () => select(2));
+            AddAssert("hall selected", () => navigation.SelectedTab == NavigationTab.Hall);
+            _ = AddStep("select store", () => select(3));
+            AddAssert("store selected", () => navigation.SelectedTab == NavigationTab.Store);
+            _ = AddStep("select settings", () => select(4));
+            AddAssert("settings selected", () => navigation.SelectedTab == NavigationTab.Settings);
+            _ = AddStep("select home", () => select(0));
+            AddAssert("home selected", () => navigation.SelectedTab == NavigationTab.Home);
+        }
+
+        private void select(int c)
+        {
+            inputManager.MoveMouseTo(navigation.Children[c].ScreenSpaceDrawQuad.Centre);
+            inputManager.Click(MouseButton.Left);
+        }
+    }
+}
---
+++ PianoTilesRedux.Game/Screens/Home/HomeNavigation/HomeNavigationItem.cs
@@ -0,0 +1,101 @@
+﻿// Piano Tiles Redux:
+// Made by tastyForReal (2022)
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Animations;
+using osu.Framework.Graphics.Textures;
+
+namespace PianoTilesRedux.Game.Screens.Home.HomeNavigation
+{
+    public class HomeNavigationItem : TextureAnimation
+    {
+        private const int total_frames = 6;
+
+        public NavigationTab Tab { get; }
+
+        private bool selected;
+
+        [Resolved]
+        private LargeTextureStore textures { get; set; }
+
+        public HomeNavigationItem(NavigationTab tab)
+        {
+            Anchor = Anchor.BottomCentre;
+            Origin = Anchor.BottomCentre;
+            Tab = tab;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            addFrames();
+            addReversedFrames();
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            if (Tab == NavigationTab.Home)
+            {
+                this.GotoAndStop(6);
+                return;
+            }
+
+            this.Stop();
+        }
+
+        private void addFrames()
+        {
+            for (int i = 0; i < total_frames; i++)
+            {
+                AddFrame(textures.Get($"HomeNavigationAnimations/{Tab}{i}"));
+            }
+        }
+
+        private void addReversedFrames()
+        {
+            for (int i = total_frames; i >= 0; i--)
+            {
+                AddFrame(textures.Get($"HomeNavigationAnimations/{Tab}{i}"));
+            }
+        }
+
+        internal void Deselect()
+        {
+            selected = false;
+
+            if (CurrentFrameIndex == 6)
+            {
+                // Add frames back to avoid an exception being thrown for when
+                // all large Texture references are lost.
+                addReversedFrames();
+                this.Play();
+            }
+        }
+
+        internal void Select()
+        {
+            selected = true;
+
+            if (CurrentFrameIndex != 6)
+            {
+                // Add frames back to avoid an exception being thrown for when
+                // all large Texture references are lost.
+                addFrames();
+                this.Restart();
+            }
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            if ((selected && CurrentFrameIndex == 6) || CurrentFrameIndex == 12)
+            {
+                this.Stop();
+            }
+        }
+    }
+}
