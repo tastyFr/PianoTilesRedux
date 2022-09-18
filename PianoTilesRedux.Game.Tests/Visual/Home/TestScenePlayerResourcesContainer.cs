8b484b02bf4daf513f4e1a4598ce595fda046b8f
tastyForReal <40455776+tastyFr@users.noreply.github.com>
2022-09-18 23:08:57 +0800

    Prefer using parameters instead of fields

--- PianoTilesRedux.Game.Tests/Visual/Home/TestScenePlayerResourcesContainer.cs
+++ PianoTilesRedux.Game.Tests/Visual/Home/TestScenePlayerResourcesContainer.cs
@@ -9,15 +9,18 @@ namespace PianoTilesRedux.Game.Tests.Visual.Home
 {
     public class TestScenePlayerResourcesContainer : PianoTilesReduxTestScene
     {
-        private PlayerResourcesContainer container;
+        private readonly PlayerResourcesContainer container;
+
+        public TestScenePlayerResourcesContainer()
+        {
+            container = new();
+            Add(container);
+        }
 
         [BackgroundDependencyLoader]
         private void load()
         {
-            container = new();
-            Add(container);
-            container.OnUpdate += _ =>
-                Schedule(() => container.Scale = new Vector2(Content.DrawSize.X / container.DrawSize.X));
+            container.OnUpdate += d => Schedule(() => d.Scale = new Vector2(Content.DrawSize.X / d.DrawSize.X));
         }
     }
 }
